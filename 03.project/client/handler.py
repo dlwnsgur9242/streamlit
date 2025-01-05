2a3ad6124e6d7d6703de73f2648d18f2f5467fab
dlwnsgur9242 <dlwnsgur9242@naver.com>
2025-01-05 20:07:19 +0900

    openai API 연결 성공

--- 03.project/client/handler.py
+++ 03.project/client/handler.py
@@ -1,15 +1,15 @@
-from openai import OpenAI
+import openai
 import os
 
 # 환경 변수에서 API 키를 가져오기
-client = OpenAI(os.environ.get("SEARCH_API_KEY"))
+openai.api_key = os.environ.get("SEARCH_API_KEY")
 
 def get_filter_keyword(user_message):
     """
     ChatGPT API를 통해 사용자의 의도를 분석하여 키워드를 추출.
     """
-    response = client.chat.completions.create(
-        model="GPT-4 Turbo",
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "너는 데이터 분석 보조 AI야."},
             {"role": "user", "content": user_message}
